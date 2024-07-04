ecd8a6203f54d0972f9b6559c0f97b0ab2a838b1
Andrei Munteanu <muntyanua@internet.ru>
2024-07-04 20:37:13 +0300

    task4primerlekcii

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,4 +1,7 @@
-﻿// int[,] matrix = new int[4, 5];
+﻿// lekcii 4 dvymernii massiv
+
+
+// int[,] matrix = new int[4, 5];
 
 // Random rnd = new Random();
 // for (int i = 0; i < matrix.GetLength(0); i++)
@@ -22,34 +25,114 @@
 
 // Primmer2
 
-int[,] CreateMatrix(int rowCount, int ColumsCount)
+// int[,] CreateMatrix(int rowCount, int ColumsCount)
+// {
+//     int[,] matrix = new int[rowCount, ColumsCount];
+
+//     Random rnd = new Random();
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+
+//             matrix[i, j] = rnd.Next(1, 11);
+//         }
+//     }
+//     return matrix;
+// }
+
+// void ShowMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+
+//             Console.Write($"{matrix[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// int[,] matrix = CreateMatrix(4,5);
+// ShowMatrix(matrix);
+
+// primer2.1
+
+// int[,] CreateMatrix(int rowCount, int ColumsCount)
+// {
+//     int[,] matrix = new int[rowCount, ColumsCount];
+
+//     Random rnd = new Random();
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+
+//             matrix[i, j] = rnd.Next(1, 1000);
+//         }
+//     }
+//     return matrix;
+// }
+
+// void ShowMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+
+//             Console.Write($"{matrix[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// int[,] matrix = CreateMatrix(3,4);
+// ShowMatrix(matrix);
+// foreach (int e in matrix)
+// {
+//     if(isInteresting(e) == true)
+//     {
+//         Console.WriteLine(e);
+//     }
+// }
+
+// bool isInteresting(int value)
+// {
+//     int sumofdigets = Getsumofdigets(value);
+//     if(sumofdigets % 2 == 0)
+//     {
+//         return true;
+//     }
+//       return false;
+// }
+// int Getsumofdigets(int value)
+// {
+//     int sum = 0;
+//     while(value > 0)
+//     {
+//         sum = sum + value % 10;
+//         value = value / 10;
+//     }
+//     return sum;
+// }
+
+// primer3
+string GetlettersFromString(string s)
 {
-    int[,] matrix = new int[rowCount, ColumsCount];
-
-    Random rnd = new Random();
-    for (int i = 0; i < matrix.GetLength(0); i++)
+    string letters = "";
+    foreach(char e in s)
     {
-        for (int j = 0; j < matrix.GetLength(1); j++)
+        if( char.IsAsciiLetter(e) == true)
         {
-
-            matrix[i, j] = rnd.Next(1, 11);
+            letters = letters + e;
         }
+
     }
-    return matrix;
+    return letters;
 }
 
-void ShowMatrix(int[,] matrix)
-{
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-
-            Console.Write($"{matrix[i, j]} ");
-        }
-        Console.WriteLine();
-    }
-}
-
-int[,] matrix = CreateMatrix(4,5);
-ShowMatrix(matrix);
+string str = Console.ReadLine();
+string result = GetlettersFromString(str);
+Console.WriteLine(result);
